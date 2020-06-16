0fb67a8108478fd5899b01ccab91c9f00a96e8d4
achrefbs <siachrefboularess@gmail.com>
2020-06-16 12:19:54 +0000

    task 1 test

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,5 +19,7 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_node(listint_t *n1, listint_t *n2, listint_t **head);
 
 #endif
