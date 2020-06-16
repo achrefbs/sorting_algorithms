752ac0a974ee41dc998d5d58a4f55a74dbe692a4
achrefbs <siachrefboularess@gmail.com>
2020-06-16 21:42:23 +0000

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+int find_minimum(int *array, int s_index, size_t size);
+/**
+ * selection_sort - sorts an array using the Selection sort algorithm.
+ * @array : array to be sorted
+ * @size : size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int i, min, tmp;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		min = find_minimum(array, i, size);
+		tmp = array[i];
+		array[i] = array[min];
+		array[min] = tmp;
+		print_array(array, size);
+	}
+}
+/**
+ * find_minimum - find index of min in array.
+ * @array : array
+ * @s_index : starting index
+ * @size : size of the array
+ * Return: index of min in array.
+ */
+int find_minimum(int *array, int s_index, size_t size)
+{
+	int i, min, min_index;
+
+	min = array[s_index];
+	min_index = s_index;
+	for (i = s_index; i < (int)size; i++)
+	{
+		if (array[i] < min)
+		{
+			min = array[i];
+			min_index = i;
+		}
+	}
+	return (min_index);
+}
